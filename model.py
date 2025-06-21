1f7e9d206598a3002cb21d678e189fb5b0320a96
Calvenn <calvenkh@gmail.com>
2025-06-21 12:20:29 +0800

    debug 3

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from flask import Flask, request, jsonify
 from PIL import Image
 import io
@@ -46,6 +46,5 @@ def home():
     return "AI Waste Classifier API is running."
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5000) run locally
-    port = int(os.environ.get('PORT', 5000))
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     app.run(host='0.0.0.0', port=port)
