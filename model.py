61e90ac11b94f2ebbfded961a6a4ac1f54e63048
Calvenn <calvenkh@gmail.com>
2025-06-21 12:13:48 +0800

    debug 2

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from PIL import Image
 import io
@@ -42,7 +43,9 @@ def predict():
 
 @app.route('/')
 def home():
-    return "✅ AI Waste Classifier API is running."
+    return "AI Waste Classifier API is running."
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000) run locally
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
