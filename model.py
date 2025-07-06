0cce609e6e53135f47fdb3206a6cab0e7094de91
Calvenn <calvenkh@gmail.com>
2025-07-06 15:29:44 +0800

    debug on backend model

--- model.py
+++ model.py
@@ -46,5 +46,5 @@ def home():
     return "AI Waste Classifier API is running."
 
 if __name__ == '__main__':
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
